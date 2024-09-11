2fcfd4e68e0fd3ebed6636e8ea3dad4d4616f115
bosco250 <dusengimana06@gmail.com>
2024-09-11 17:33:20 +0200

    ch

--- src/Components/Layout/NavBar.jsx
+++ src/Components/Layout/NavBar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import Log from "../../assets/Logo.png";
 import { FaXmark } from "react-icons/fa6";
-import { IoSearch } from "react-icons/io5";
 import { IoIosNotifications } from "react-icons/io";
 import { Link } from "react-router-dom";
 
@@ -13,7 +12,7 @@ const Navbar = () => {
   };
   return (
     <div
-  className="md:fixed md:w-full top-0 left-0 bg-gray-900 z-10
+  className="md:fixed md:w-full top-0 left-0 bg-[#184b41] z-10
     py-[30px] px-[50px] md:flex items-center justify-center text-gray-200 h-[13vh]"
 >
       <div className="md:flex md:flex-row gap-10 justify-between items-center w-full hidden">
@@ -27,12 +26,12 @@ const Navbar = () => {
           <div>
             <ul className="nav-items flex md:flex-row list-none text-[20px]
             gap-[35px] justify-center items-center font-bold leading-6">
-              <li className="cursor-pointer hover:text-green-600"><Link to='/'>Home</Link></li>
-              <li className="cursor-pointer hover:text-green-600"> <Link to='/about'>About</Link> </li>
-              <li className="cursor-pointer hover:text-green-600"><Link to='/dashboard'>Dashboard</Link> </li>
-              <li className="cursor-pointer hover:text-green-600">Projects</li>
-              <li className="cursor-pointer hover:text-green-600">Routes</li>
-              <li  className="cursor-pointer hover:text-green-600"><IoIosNotifications className="h-6 w-6" />              </li>
+              <li className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200"><Link to='/'>Home</Link></li>
+              <li className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200"> <Link to='/about'>About</Link> </li>
+              <li className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200"><Link to='/dashboard'>Dashboard</Link> </li>
+              <li className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200">Projects</li>
+              <li className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200">Routes</li>
+              <li  className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200"><IoIosNotifications className="h-6 w-6" />              </li>
             </ul>
           </div>
         </div>
@@ -41,7 +40,7 @@ const Navbar = () => {
       {/* For Mobile navBar */}
       <div
         className="flex flex-row md:hidden fixed top-0 left-0
-      justify-between items-center h-24 z-10 w-full px-5 bg-gray-900"
+      justify-between items-center h-24 z-10 w-full px-5 bg-[#184b41]"
       >
         <div className="flex justify-center items-center">
           <img className="w-8 h-10 object-fill" src={Log} alt="" />
@@ -61,21 +60,21 @@ const Navbar = () => {
       </div>
       {/* mobile items */}
       <div
-        className={`fixed bg-gray-900 z-10 h-full py-7 top-24 left-0
+        className={`fixed bg-[#184b41] z-10 h-full py-7 top-24 left-0
             ${
               isOpen ? " w-full transition-all ease-out duration-500" : "hidden"
             } `}
       >
-        <ul
+        <ul 
           className={`nav-items flex flex-col list-none text-[20px]
             gap-[25px] justify-center px-6 font-bold leading-6`}
         >
-          <li className="cursor-pointer hover:text-green-600">Home</li>
-          <li className="cursor-pointer hover:text-green-600">About</li>
-          <li className="cursor-pointer hover:text-green-600"><Link to='/dashboard'>Dashboard</Link> </li>
-          <li className="cursor-pointer hover:text-green-600">Projects</li>
-          <li className="cursor-pointer hover:text-green-600">Routes</li>
-          <li className=" cursor-pointer hover:text-green-600">Notifications</li>
+          <li onClick={()=>setIsOpen(!isOpen)} className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200"><Link to='/'>Home</Link> </li>
+          <li onClick={()=>setIsOpen(!isOpen)} className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200"> <Link to='/about'>About</Link> </li>
+          <li onClick={()=>setIsOpen(!isOpen)} className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200"><Link to='/dashboard'>Dashboard</Link> </li>
+          <li onClick={()=>setIsOpen(!isOpen)} className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200">Projects</li>
+          <li onClick={()=>setIsOpen(!isOpen)} className="cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200">Routes</li>
+          <li onClick={()=>setIsOpen(!isOpen)} className=" cursor-pointer px-3 py-2 rounded-md hover:bg-green-700 hover:text-green-200">Notifications</li>
         </ul>
       </div>
     </div>
